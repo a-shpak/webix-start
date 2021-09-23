c8a4d19ede19299525d7d5d2b51bc02c454bbd18
AndreyShpakIT <andrey.shpak.it@gmail.com>
2021-09-23 21:28:01 +0300

    profile popup

--- js/header.js
+++ js/header.js
@@ -6,6 +6,18 @@ const header = {
     cols: [
         { view:"label", label:"My App", css:"title" },
         {},
-        { view:"button", label:"Profile", type:"icon", icon:"wxi-user", css:"webix_transparent", maxWidth:200, id:"button_header" }
+        { view:"button", label:"Profile", type:"icon", icon:"wxi-user", 
+        css:"webix_transparent", maxWidth:200, id:"button_header", popup:"profile_popup" }
     ]
+};
+
+const profile_popup = {
+    view:"popup",
+    id:"profile_popup",
+    width:"300",
+    body: {
+        view:"list",
+        data: [ "Settings", "Log out"],
+        autoheight:true
+    }
 };
