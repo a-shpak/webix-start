8d8ec8ced4232ccb6b637a439da23efaf1317b29
AndreyShpakIT <andrey.shpak.it@gmail.com>
2021-09-27 23:01:09 +0300

    products tab done

--- js/main/products/treetable.js
+++ js/main/products/treetable.js
@@ -1,10 +1,10 @@
 export const productsTreetable = {
-    id:"products_treetable",
+    id:"treetable_products",
     view:"treetable",
     columns:[
         { id:"id", header:"", width:50 },
-        { id:"title", header:"Title", fillspace:true, template:"{common.treetable()} #title#" },
-        { id:"price", header:"Price" },
+        { id:"title", header:"Title", fillspace:true, template:"{common.treetable()} #title#", editor:"text" },
+        { id:"price", header:"Price", editor:"text" },
     ],
     scroll:"y",
     select:"cell",
@@ -12,5 +12,10 @@ export const productsTreetable = {
         onAfterLoad:function() {
             this.openAll();
         }
+    },
+    editable:true,
+    rules:{
+        "title":value => !!value,
+        "price":value => !!value && !isNaN(value) && value >= 0,
     }
 };
